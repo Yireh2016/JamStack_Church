7e2831687409b2841ef5eb9dd5df7ad6d07b233a
yireh2016 <jainer.calvetti@gmail.com>
2020-04-16 20:57:29 -0400

    chore: about main image background

--- src/themes/mainTheme.js
+++ src/themes/mainTheme.js
@@ -1,9 +1,10 @@
 export const mainTheme = {
   color: {
-    dorado: '#EDB955',
-    blanco: '#f2f2f2',
-    violetAlpha: 'rgba(36, 23, 54, 0.89)',
-    violet: 'rgba(36, 23, 54, 1)',
+    dorado: `#EDB955`,
+    blanco: `#f2f2f2`,
+    violetAlpha: `rgba(36, 23, 54, 0.89)`,
+    violet: `rgba(36, 23, 54, 1)`,
+    negroAlpha: `rgba(4, 4, 4, 0.81)`
   },
-  font: 'Montserrat, sans-serif',
+  font: `Montserrat, sans-serif`
 }
